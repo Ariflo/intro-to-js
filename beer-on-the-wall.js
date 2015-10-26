0a17e0dc6aa93b40259f0bfa60b919d55da8ce48
AriFlo <ariflo@arianflores.com>
2015-10-26 13:38:17 -0700

    completed beer-on-the-wall.js

---
+++ beer-on-the-wall.js
@@ -0,0 +1,22 @@
+
+var beerSong = function(numBeers) {
+
+		if(numBeers === 1){
+			console.log( numBeers + " bottle of beer on the wall");
+		 	console.log( numBeers + " bottle of beer");	
+
+	 		console.log( "Take it down and pass it around, No more bottles of beer on the wall.\n" );
+	 	}
+	 	else{
+	 	console.log( numBeers + " bottles of beer on the wall");
+	 	console.log( numBeers + " bottles of beer"); 
+
+	 	var oneLess = numBeers -1; 
+	 	console.log( "Take one down and pass it around, " + oneLess +  " bottles of beer on the wall. \n");
+
+	 	beerSong(oneLess);
+	 	}
+} 
+
+beerSong(10); 
+
